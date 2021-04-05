864bc0c9084de600f980ca2ae862224b2ccd61a8
Sean <sryan@exponent.com>
2021-04-05 10:24:03 -0700

    Add description for params data

---
+++ Casanova/R/params_units.R
@@ -0,0 +1,16 @@
+#' Example data format for params file
+#'
+#' Description of parameters and required units in metric and English system.
+#'
+#' @docType data
+#'
+#' @usage data(params)
+#'
+#' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
+#'
+#' @keywords datasets
+#'
+#'
+#' @examples
+#' data(params)
+"params"
