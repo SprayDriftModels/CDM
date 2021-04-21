88ef23309aa4d8e5609160093cc36cea04820a45
Sean <sryan@exponent.com>
2021-04-21 07:06:19 -0700

    remove wind/height measurements; housekeeping

--- Casanova/R/create_paramsData.R
+++ Casanova/R/create_paramsData.R
@@ -3,9 +3,6 @@ create_paramsData <- function(
     Patm,
     RH,
     ch,
-    WTmeasurements,
-    z1,
-    ux1,
     psipsipsi,
     psipsipsi_method,
     rhow,
@@ -22,99 +19,70 @@ create_paramsData <- function(
     PL,
     NozzleSpacing,
     MMM,
-    lambda) {
+    lambda,
+    units) {
 
 
-  paramsData <- tibble(
-    Type = c(
-      "Tair",
-      "Patm",
-      "RH",
-      "ch",
-      "WTmeasurements",
-      "z1",
-      "ux1",
-      "psipsipsi",
-      "psipsipsi_method",
-      "rhow",
-      "rhos",
-      "xs0",
-      "rhosoln",
-      "H0",
-      "hcm",
-      "app_p",
-      "angle",
-      "IAR",
-      "xactive",
-      "FD",
-      "PL",
-      "NozzleSpacing",
-      "MMM",
-      "lambda"
-    ),
-    Value_1 = c(
-      Tair,
-      Patm,
-      RH,
-      ch,
-      WTmeasurements,
-      z1,
-      ux1,
-      psipsipsi,
-      psipsipsi_method,
-      rhow,
-      rhos,
-      xs0,
-      rhosoln,
-      H0,
-      hcm,
-      app_p,
-      angle,
-      IAR,
-      xactive,
-      FD,
-      PL,
-      NozzleSpacing,
-      MMM,
-      lambda
+  if (units == "Metric") {
+    descr_units <- Casanova::params_metric %>% select(Description, Units)
+  } else {
+    descr_units <- Casanova::params_english %>% select(Description, Units)
+  }
+
+  paramsData <-
+    tibble(
+      Type = c(
+        "ID",
+        "Tair",
+        "Patm",
+        "RH",
+        "ch",
+        "psipsipsi",
+        "psipsipsi_method",
+        "rhow",
+        "rhos",
+        "xs0",
+        "rhosoln",
+        "H0",
+        "hcm",
+        "app_p",
+        "angle",
+        "IAR",
+        "xactive",
+        "FD",
+        "PL",
+        "NozzleSpacing",
+        "MMM",
+        "lambda"
+      )
+    ) %>%
+    bind_cols(descr_units) %>%
+    mutate(
+      Value_1 = c(
+        1,
+        Tair,
+        Patm,
+        RH,
+        ch,
+        psipsipsi,
+        psipsipsi_method,
+        rhow,
+        rhos,
+        xs0,
+        rhosoln,
+        H0,
+        hcm,
+        app_p,
+        angle,
+        IAR,
+        xactive,
+        FD,
+        PL,
+        NozzleSpacing,
+        MMM,
+        lambda
+      )
     )
-  )
-
-
-#***SFR do we need to add the units column???
-
-
-  ##***SFR another approach
-  ## Transpose
-
-  # paramsData <- tibble(
-  #   Tair = Tair,
-  #   Patm = Patm,
-  #   RH = RH,
-  #   ch = ch,
-  #   NumberMeasures_chosen = NumberMeasures_chosen,
-  #   z1 = z1,
-  #   ux1 = ux1,
-  #   psipsipsi = psipsipsi,
-  #   psipsipsi_method = psipsipsi_method,
-  #   rhow = rhow,
-  #   rhos = rhos,
-  #   xs0 = xs0,
-  #   rhosoln = rhosoln,
-  #   H0 = H0,
-  #   hcm = hcm,
-  #   app_p = app_p,
-  #   angle = angle,
-  #   IAR = IAR,
-  #   xactive = xactive,
-  #   FD = FD,
-  #   PL = PL,
-  #   NozzleSpacing = NozzleSpacing,
-  #   MMM = MMM,
-  #   lambda = lambda
-  # )
-  # paramsData = setNames(data.frame(t(paramsData[,])), paramsData[,1])
-  # colnames(paramsData) <- c("Type", "Value_1")
 
   return(paramsData)
 
