90b844db2347865a38a942be0b1e34264deecaf3
John Buonagurio <jbuonagurio@exponent.com>
2023-08-16 18:36:24 -0400

    Use shorter variable names for molecular weight constants.

--- src/Constants.hpp
+++ src/Constants.hpp
@@ -12,10 +12,10 @@ namespace constants {
 constexpr double karman = 0.40;
 
 // Molecular weight of water [g/mol]
-constexpr double mol_wt_water = 1.00794 * 2. + 15.9994;
+constexpr double mww = 1.00794 * 2. + 15.9994;
 
 // Molecular weight of air [g/mol]
-constexpr double mol_wt_air = 0.209 * 2. * 15.9994 + (1. - 0.209) * 2. * 14.007;
+constexpr double mwa = 0.209 * 2. * 15.9994 + (1. - 0.209) * 2. * 14.007;
 
 // Distance between nozzle and liquid sheet [m]
 constexpr double liquid_sheet_offset = 0.1016;
